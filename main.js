fbd4668cf235042d0f76384766dd3881882a1857
Vu Thu Giang <vuthugiang2610@gmail.com>
2021-06-18 11:39:46 +0700

    :>

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+function getEle(id) {
+  return document.getElementById(id);
+}
 // bai 1
 var n = 0;
 var i = 1;
@@ -5,4 +8,27 @@ while (n <= 10000) {
   n += i;
   i++;
 }
-console.log(--i);
+getEle('bai1').innerHTML = 'Số nguyên dương nhỏ nhất: ' + --i;
+// bai 2
+function tinhTong(e) {
+  e.preventDefault();
+  var bai2 = getEle('bai2');
+  var x = getEle('x').value;
+  var n = getEle('n').value;
+  if (x === '') {
+    bai2.innerHTML = 'Vui lòng nhập x';
+  } else if (n === '') {
+    bai2.innerHTML = 'Vui lòng nhập n';
+  } else if (n <= 0) {
+    bai2.innerHTML = 'Vui lòng nhập n lớn hơn 0';
+  } else {
+    var result = 0;
+    for (var i = 1; i <= n; i++) {
+      result += Math.pow(x, n);
+    }
+    bai2.innerHTML = `Kết quả của phép toán là: ${result}`;
+  }
+}
+
+// bai 3
+var num = 6;
